bde28c049d3e48afbd0462bbc1f3949ebd6288b1
shaiksshahbaz <46316225+shaiksshahbaz@users.noreply.github.com>
2020-01-20 15:36:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=$(date +%H-%M-%S)
-docker build -t sreeharshav/testingbuild:${VERSION} .
+docker build -t shaikshahbaz/testingbuild:${VERSION} .
 docker push shaikshahbaz/testingbuild:${VERSION}
 docker -H tcp://10.1.2.184:2375 stop nginx
 docker -H tcp://10.1.2.184:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx sreeharshav/testingbuild:${VERSION}
